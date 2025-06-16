19f50fce5df061805e47fcd604df840701b72b88
argyadwi <argyawoles@gmail.com>
2025-06-16 15:30:58 +0700

    fix seller

--- resources/views/web/customer/cart/index.blade.php
+++ resources/views/web/customer/cart/index.blade.php
@@ -228,16 +228,6 @@
                                         Checkout Sekarang
                                     </button>
                                 </form>
-                                <button type="button"
-                                    class="inline-flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 transition-colors bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor"
-                                        viewBox="0 0 24 24">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
-                                        </path>
-                                    </svg>
-                                    Unduh Invoice
-                                </button>
                             </div>
                         </div>
                     </div>
